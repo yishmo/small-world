e90f270af2f75ba63f8895c359a3dd632e06e1a3
yishmo <ymcnabb@uci.edu>
2021-10-10 23:03:56 -0700

    beta version

--- sw_card_finder.py
+++ sw_card_finder.py
@@ -1,26 +1,25 @@
 import requests
-import sys
+import os
 
+from gabesw import run_gabes_code
 from ygo_api import get_all_monsters_for_small_world
 
-#best to use a global variable
-#since we want to query the API as
-#little as possible
+# best to use a global variable
+# since we want to query the API as
+# little as possible
 response_json = get_all_monsters_for_small_world()
+
 
 def given_card_name_get_attrs(cardName):
     attr_dict = {}
-    for card in response_json['data']:
-        if cardName == card['name']:
+    for card in response_json["data"]:
+        if cardName == card["name"]:
             attr_dict["atk"] = card["atk"]
             attr_dict["def"] = card["def"]
             attr_dict["attribute"] = card["attribute"]
             attr_dict["level"] = card["level"]
             attr_dict["race"] = card["race"]
 
-    if attr_dict == {}:
-        print(cardName, 'is not the full name of a Yu-Gi-Oh! card, please check your spelling and try again.')
-        exit()
     return attr_dict
 
 
@@ -47,6 +46,7 @@ def card_popularity_function(card_api_resp):
 
     return total
 
+
 def find_bridge(name1, name2):
     attrs1 = given_card_name_get_attrs(name1)
     attrs2 = given_card_name_get_attrs(name2)
@@ -64,6 +64,17 @@ def find_bridge(name1, name2):
     for i in range(len(sorted_list)):
         print(f'{i + 1}. {sorted_list[i]["name"]}')
 
+def verify_card_legality(cardName):
+    attr_dict = given_card_name_get_attrs(cardName)
+    if attr_dict == {}:
+        print(
+            cardName,
+            "is not the full name of a Yu-Gi-Oh! monster card, please check your spelling and try again (name is case sensitive).",
+        )
+        return False
+    return True
+
+
 def find_small_world_compatible(name):
     attrs = given_card_name_get_attrs(name)
 
@@ -76,23 +87,48 @@ def find_small_world_compatible(name):
     for i in range(len(sorted_list)):
         print(f'{i + 1}. {sorted_list[i]["name"]}')
 
+
 def main():
     while True:
-        mode = input('\nWhich mode would you like to use? Type in the corresponding number and press enter or press q to quit.\n\n1 Banish card A from hand, reveal card B from deck, add card C to hand ... I have cards A and C and want to find B.\n2 I want to know all cards that share extacly one thing (ATK, DEF, Level, Type, Attribute) with a card.\n')
+        mode = input(
+"""
+Which mode would you like to use? Type in the corresponding number and press 
+enter or press (q) to quit.
 
-        if mode == '1':
-            card1 = input('Enter the name of the card in your hand: ')
-            card2 = input('Enter the name of the card you want to search: ')
+1. Banish card A from hand, reveal card B from deck, 
+   add card C to hand ... I have cards A and C and want to find B.
 
-            find_bridge(card1, card2)
-        elif mode == '2':
-            card = input('Enter the name of the card to find all Small World compatible cards: ')
-            find_small_world_compatible(card)
+2. I want to know all cards that share extacly one thing (ATK, DEF, Level, Type, Attribute) 
+   with a card.
 
-        elif mode == 'q':
+3. I want to see how well Small World works in my deck (Gabriel 
+   Netz wrote most of this code).
+
+Your Choice: """
+        )
+
+        if mode == "1":
+            card1 = input("Enter the name of the monster card in your hand: ")
+            card2 = input("Enter the name of the monster card you want to search: ")
+            print('--------------')
+            if verify_card_legality(card1) and verify_card_legality(card2):
+                find_bridge(card1, card2)
+        elif mode == "2":
+            card = input(
+                "Enter the name of a monster card to find all Small World compatible cards: "
+            )
+            print('--------------')
+            if verify_card_legality(card):
+                find_small_world_compatible(card)
+        elif mode == "3":
+            print('--------------')
+            run_gabes_code()
+
+        elif mode == "q":
             break
         else:
-            print('please enter 1, 2 or q')
+            print("please enter 1, 2 or q")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
