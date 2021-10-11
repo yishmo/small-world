a1c1243894149545b2c2ca4bf0edc9330b5eb255
yishmo <ymcnabb@uci.edu>
2021-10-10 23:05:16 -0700

    add gabe/my hybrid code

---
+++ gabesw.py
@@ -0,0 +1,74 @@
+# Small World Simulator
+
+import requests
+import json
+import pprint
+
+from ygo_api import get_all_monsters_for_small_world
+
+
+def getScore(card, comparison):
+    score = 0
+    for key in card:
+        if card[key] == comparison[key]:
+            score = score + 1
+    return score
+
+def run_gabes_code():
+    user_input = input(
+        "Copy and paste your .ydk file main deck card numbers (all numbers after #main and before #side then press enter: "
+    )
+    user_input.strip()
+    # length of every card id
+    n = 8
+    deck = [int(user_input[i : i + n]) for i in range(0, len(user_input), n)]
+    # remove duplicates
+    deck = set(list(deck))
+
+    deckmonsters = {}
+    monsterbridges = {}
+
+    response_json = get_all_monsters_for_small_world()
+
+    # Step 2: API Calls
+    for card in deck:
+        for info in response_json["data"]:
+            if card == info["id"]:
+                deckmonsters[info["name"]] = {
+                    "ATK": info["atk"],
+                    "DEF": info["def"],
+                    "Attribute": info["attribute"],
+                    "Type": info["race"],
+                    "Level": info["level"],
+                }
+
+    monster_names = set(deckmonsters.keys())
+
+    # Step 3: Actual Logic
+
+
+    for card in deckmonsters:
+        monsterbridges[card] = []
+        for key in deckmonsters:
+            score = getScore(deckmonsters[card], deckmonsters[key])
+            if score == 1:
+                monsterbridges[card].append(key)
+
+
+    # Right Now, monster-bridges is a dict with all cards that connect to each other. Now, we want to output all the cards each card can search
+
+    missing = {}
+    print("---ALL POSSIBLE SMALL WORLD ADDS---")
+    with open("output.txt", "w") as f:
+        for card in monsterbridges:
+            targets = set()
+            for key in monsterbridges[card]:
+                for target in monsterbridges[key]:
+                    print(f"Banish {card} ---> Reveal {key} ---> Add {target}")
+                    f.write(f"Banish {card} ---> Reveal {key} ---> Add {target}\n")
+                    targets.add(target)
+            missing[card] = monster_names - targets - {card}
+
+    print()
+    print("---ALL MISSING LINKS---")
+    pprint.pprint(missing)
